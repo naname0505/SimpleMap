8f88aa224e4f70bf5fc3f050134bb530cdeedc21
naname0505 <naname84j@gmail,.com>
2017-07-03 16:28:32 +0900

    add min distance display

--- app/src/main/java/jp/ac/titech/itpro/sdl/simplemap/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/simplemap/MainActivity.java
@@ -20,8 +20,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.app.Activity;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import android.location.Location;
 
-
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
@@ -40,8 +45,10 @@ import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
-
+import com.google.android.gms.maps.SupportMapFragment;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity implements
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
@@ -62,6 +69,11 @@ public class MainActivity extends Activity implements
     private final static int REQCODE_PERMISSIONS = 1111;
     double Lat;
     double Long;
+    private Marker gMarker = null;
+    private ArrayList<String> Llatilong;
+    int n = 1;
+    String LatiLongDis [] = new String [30]; //0:1lat, 1;1long, 2;1dis, 3:2lat...
+    float max_dis = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,29 +83,143 @@ public class MainActivity extends Activity implements
         drawer.openDrawer(android.support.v4.view.GravityCompat.START);
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map_fragment);
+
+
+        android.support.design.widget.NavigationView navigationView = (android.support.design.widget.NavigationView) findViewById(R.id.nav_view);
+        View header=navigationView.getHeaderView(0);
+        final TextView email_tv = (TextView)header.findViewById(R.id.email);
+        final TextView name_tv = (TextView)header.findViewById(R.id.name);
+        name_tv.setText("現在地からの最短距離");
+        email_tv.setText(String.valueOf(max_dis));
+
+        final android.view.Menu menuNav = navigationView.getMenu();
+        final android.view.MenuItem share_1 = menuNav.findItem(R.id.nav_1);
+        final android.view.MenuItem share_2 = menuNav.findItem(R.id.nav_2);
+        final android.view.MenuItem share_3 = menuNav.findItem(R.id.nav_3);
+        final android.view.MenuItem share_4 = menuNav.findItem(R.id.nav_4);
+        final android.view.MenuItem share_5 = menuNav.findItem(R.id.nav_5);
+        final android.view.MenuItem share_6 = menuNav.findItem(R.id.nav_6);
+        final android.view.MenuItem share_7 = menuNav.findItem(R.id.nav_7);
+        final android.view.MenuItem share_8 = menuNav.findItem(R.id.nav_8);
+        final android.view.MenuItem share_9 = menuNav.findItem(R.id.nav_9);
+        final android.view.MenuItem share_10 = menuNav.findItem(R.id.nav_10);
+
+
+
+
+
+
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap map) {
-                map.moveCamera(CameraUpdateFactory.zoomTo(15f));
+                map.moveCamera(CameraUpdateFactory.zoomTo(15));
                 googleMap = map;
                 // タップした時のリスナーをセット
                 googleMap.setOnMapClickListener(new OnMapClickListener() {
                     @Override
                     public void onMapClick(LatLng tapLocation) {
-                      // tapされた位置の緯度経度
-                      location = new LatLng(tapLocation.latitude, tapLocation.longitude);
-                      googleMap.addMarker(new MarkerOptions().position(location).title(""+tapLocation.latitude+" :"+ tapLocation.longitude));
-                      googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f));
-                      }
-                    });
+                        // tapされた位置の緯度経度
+                        //location = new LatLng(tapLocation.latitude, tapLocation.longitude);
+                        //googleMap.addMarker(new MarkerOptions().position(location).title(""+tapLocation.latitude+" :"+ tapLocation.longitude));
+                        // googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f));
+                        /*
+                        // TODO Auto-generated method stub
+                        String id = .getId();
+                        if (id.equals("m0")) {
+                            // マーカー削除
+                            gMarker.remove();
+                            toast.makeText(getApplicationContext(), "マーカー削除!", Toast.LENGTH_LONG).show();
+                        }
+                        */
+                    }
+                });
 
                 // 長押しのリスナーをセット
                 googleMap.setOnMapLongClickListener(new OnMapLongClickListener() {
                     @Override
                     public void onMapLongClick(LatLng long_pushLocation) {
-                      LatLng new_location = new LatLng(long_pushLocation.latitude, long_pushLocation.longitude);
-                      googleMap.addMarker(new MarkerOptions().position(new_location).title(""+long_pushLocation.latitude+" :"+ long_pushLocation.longitude));
-                      googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f));
+                        LatLng new_location = new LatLng(long_pushLocation.latitude, long_pushLocation.longitude);
+                        gMarker = googleMap.addMarker(new MarkerOptions().position(new_location).title(""+long_pushLocation.latitude+" :"+ long_pushLocation.longitude));
+                        CameraPosition cameraPos = new CameraPosition.Builder().target(long_pushLocation).zoom(15).build();
+                        MarkerOptions options = new MarkerOptions();
+                        options.position(long_pushLocation);
+                        // TODO Auto-generated method stub
+                        // タッチ地点と目的地との最短距離の計算
+                        float[] results = new float[1];
+                        Location.distanceBetween(long_pushLocation.latitude, long_pushLocation.longitude, Lat, Long, results);
+                        int result = java.lang.Float.compare(results[0]/1000,max_dis);
+                        if(result == -1) {
+                            if(String.valueOf(max_dis) == String.valueOf(10000.0)){
+                                email_tv.setText(String.valueOf("no set"));
+                            }
+                            max_dis = results[0]/1000;
+                            email_tv.setText(String.valueOf(String.format("%.4f", max_dis))+"Km");
+                        }
+                        LatiLongDis[n]   = String.valueOf(String.format("%.6f",long_pushLocation.latitude));
+                        LatiLongDis[n+1] = String.valueOf(String.format("%.6f",long_pushLocation.longitude));
+                        LatiLongDis[n+2] = String.valueOf(String.format("%.4f", results[0]/1000));
+
+                        switch (n){
+                            case 1:
+                                share_1.setTitle("目的地まで : "+LatiLongDis[n+2]+"Km");
+                                BitmapDescriptor icon_1 = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE);
+                                options.icon(icon_1);
+                                break;
+                            case 2:
+                                share_2.setTitle("目的地まで : "+LatiLongDis[n+2]+"Km");
+                                BitmapDescriptor icon_2 = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE);
+                                options.icon(icon_2);
+                                break;
+                            case 3:
+                                share_3.setTitle("目的地まで : "+LatiLongDis[n+2]+"Km");
+                                BitmapDescriptor icon_3 = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN);
+                                options.icon(icon_3);
+                                break;
+                            case 4:
+                                share_4.setTitle("目的地まで : "+LatiLongDis[n+2]+"Km");
+                                BitmapDescriptor icon_4 = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN);
+                                options.icon(icon_4);
+                                break;
+                            case 5:
+                                share_5.setTitle("目的地まで : "+LatiLongDis[n+2]+"Km");
+                                BitmapDescriptor icon_5 = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA);
+                                options.icon(icon_5);
+                                break;
+                            case 6:
+                                share_6.setTitle("目的地まで : "+LatiLongDis[n+2]+"Km");
+                                BitmapDescriptor icon_6 = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE);
+                                options.icon(icon_6);
+                                break;
+                            case 7:
+                                share_7.setTitle("目的地まで : "+LatiLongDis[n+2]+"Km");
+                                BitmapDescriptor icon_7 = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED);
+                                options.icon(icon_7);
+                                break;
+                            case 8:
+                                share_8.setTitle("目的地まで : "+LatiLongDis[n+2]+"Km");
+                                BitmapDescriptor icon_8 = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE);
+                                options.icon(icon_8);
+                                break;
+                            case 9:
+                                share_9.setTitle("目的地まで : "+LatiLongDis[n+2]+"Km");
+                                BitmapDescriptor icon_9 = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET);
+                                options.icon(icon_9);
+                                break;
+                            case 10:
+                                share_10.setTitle("目的地まで : "+LatiLongDis[n+2]+"Km");
+                                BitmapDescriptor icon_10 = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW);
+                                options.icon(icon_10);
+                                break;
+                            case 11:
+                                Toast.makeText(getApplicationContext(), "欲張りすぎ...", Toast.LENGTH_LONG).show();
+                                n = 0;
+                                break;
+
+                        }
+                        googleMap.addMarker(options);
+                        n = n + 1;
+                        Toast.makeText(getApplicationContext(), "現在地からの距離：" + ( (Float)(results[0]/1000) ).toString() + "Km", Toast.LENGTH_LONG).show();
+
                     }
                 });
 
